ab7911600b94357a2f714d189e0708e7e35aba07
tugba <tberk@imobilecode.com>
2020-04-15 22:35:35 +0300

    Back button was developed

--- Youtube/app/src/main/java/com/example/youtube/MainActivity.kt
+++ Youtube/app/src/main/java/com/example/youtube/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.youtube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -22,15 +24,28 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation.setupWithNavController(navController)
         setupActionBarWithNavController(this, navController)
 
+        val appBarConfiguration = AppBarConfiguration(bottomNavigation.menu)
+        setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.turkeyFragment, R.id.germanyFragment, R.id.usaFragment -> {
+                    bottomNavigation.isVisible = true
+                }
+                else -> bottomNavigation.isVisible = false
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(
-            Navigation.findNavController(
-                this,
-                R.id.mainNavHost
-            ), null
-        )
+        super.onBackPressed()
+        return true
+    }
+    override fun onBackPressed() {
+        when (navController.currentDestination?.id) {
+            R.id.videoDetailFragment ->super.onBackPressed()
+            else ->  finish()
+        }
     }
 
 
